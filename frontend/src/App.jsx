838b225121a157eb4c476d2c396242be70fcb268
davidskottke <david.skottke@neuefische.de>
2023-04-28 15:11:07 +0200

    Add Hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 
 import Navbar from "./Navbar.jsx";
+import Hero from "./Hero.jsx";
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
     return (
         <div className="App">
             <Navbar/>
-
+            <Hero/>
         </div>
     )
 }
---
+++ frontend/src/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero(props) {
+    return (
+        <section className={"hero-container"}>
+            <div>
+                <h1>ToDo-Application</h1>
+                <p>Stay organized and focused with our easy-to-use Todo list.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
