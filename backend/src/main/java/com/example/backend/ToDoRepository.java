fbb67fb76a7b8b7e4c151ae4ebe91b7ccf020f15
davidskottke <david.skottke@neuefische.de>
2023-05-03 11:53:48 +0200

    Add Repository annotation

--- backend/src/main/java/com/example/backend/ToDoRepository.java
+++ backend/src/main/java/com/example/backend/ToDoRepository.java
@@ -2,6 +2,8 @@ package com.example.backend;
 
 import com.example.backend.model.ToDo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoRepository extends MongoRepository<ToDo,String> {
 }
