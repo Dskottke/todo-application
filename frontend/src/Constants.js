ce5a894ccd37a2dd5ba09de633167f325fcda201
davidskottke <david.skottke@neuefische.de>
2023-05-04 16:11:07 +0200

    Add min date

---
+++ frontend/src/Constants.js
@@ -0,0 +1,4 @@
+export const IN_PROGRESS = "IN_PROGRESS"
+export const OPEN = "OPEN"
+export const DONE = "DONE"
+export const minDate = new Date().toISOString().split("T")[0]
--- frontend/src/types.js
+++
@@ -1,3 +0,0 @@
-export const IN_PROGRESS = "IN_PROGRESS"
-export const OPEN = "OPEN"
-export const DONE = "DONE"
