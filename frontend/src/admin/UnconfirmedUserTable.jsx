9289bcc9fc57a41640103b3a7b715e8dd93cf30e
davidskottke <david.skottke@neuefische.de>
2023-05-15 17:26:31 +0200

    Add jQuery for fetching unconfirmed user

--- frontend/src/admin/UnconfirmedUserTable.jsx
+++ frontend/src/admin/UnconfirmedUserTable.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import UnconfirmedUserRow from "./UnconfirmedUserRow.jsx";
-import {useGlobalContext} from "../Context.jsx";
+import {useUnconfirmedUsersFetch} from "../hooks.js";
 
-function UnconfirmedUserTable(props) {
-    const {unconfirmedUsers} = useGlobalContext()
-    return (
-        <section className={"unconfirmed-table"}>
-            <h1>Unconfirmed users</h1>
-            <div className={"head_underline"}/>
-            <table>
-                <thead>
-                <tr>
-                    <th>username</th>
-                    <th>role</th>
-                    <th>confirm</th>
-                </tr>
-                </thead>
-                <tbody>
-                {unconfirmedUsers.map((user) => {
-                    return <UnconfirmedUserRow key={user.id} {...user}/>
-                })
-                }
-                </tbody>
-            </table>
-        </section>
-    );
+function UnconfirmedUserTable() {
+    const {data, isLoading, isError, isSuccess} = useUnconfirmedUsersFetch()
+
+    if (isLoading) return <h1>Loading...</h1>
+    if (isError) return <h1>Error...</h1>
+    if (isSuccess) {
+
+        const users = data.data
+        return (
+            <section className={"unconfirmed-table"}>
+                <h1>Unconfirmed users</h1>
+                <div className={"head_underline"}/>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>username</th>
+                        <th>role</th>
+                        <th>confirm</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {users.map((user) => {
+                        return <UnconfirmedUserRow key={user.id} {...user}/>
+                    })
+                    }
+                    </tbody>
+                </table>
+            </section>
+        );
+    }
 }
 
 export default UnconfirmedUserTable;
