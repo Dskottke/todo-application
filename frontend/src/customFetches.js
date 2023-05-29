8e191c6afaffe454501b3d8d6f3802797f098a42
davidskottke <david.skottke@neuefische.de>
2023-05-29 18:44:52 +0200

    Add with credentials

--- frontend/src/customFetches.js
+++ frontend/src/customFetches.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const customToDoFetch = axios.create({
-    baseURL: "/api/todo"
-});
+        baseURL: "/api/todo"
+    },
+    {withCredentials: true});
 const customUserFetch = axios.create({
-    baseURL: "/api/user"
-});
+        baseURL: "/api/user"
+    },
+    {withCredentials: true});
 
 export default {customToDoFetch, customUserFetch};
 
